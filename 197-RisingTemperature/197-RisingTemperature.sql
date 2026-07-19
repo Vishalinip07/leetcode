9a05d49c9c8e96a5c89ce4c87b70d04328fd5694
Vishalinip07 <vishababy07@gmail.com>
2026-07-19 21:07:05 +0530

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,9 @@
+-- Last updated: 19/07/2026, 21:07:01
+# Write your MySQL query statement below
+SELECT today.id
+FROM Weather yesterday 
+CROSS JOIN Weather today
+
+WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature
+;
