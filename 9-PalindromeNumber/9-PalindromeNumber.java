be8e55dfd3faff015c389420ab28bc1ed3e7bca1
Vishalinip07 <vishababy07@gmail.com>
2026-07-19 21:08:49 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 19/07/2026, 21:08:46
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 ==0 && x!=0)){
+            return false;
+        } 
+        int reversedHalf =0;
+        while(x>reversedHalf){
+            int digit = x%10;
+            reversedHalf = reversedHalf *10 +digit;
+            x = x/10;
+        }
+        return (x == reversedHalf) || (x == reversedHalf / 10);
+    }
+}
